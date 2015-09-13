1ee18cd2c94db3b950fa8b7b76adea6f09b10955
ZulDahalin <zul@uum.edu.my>
2015-09-13 10:20:15 +0800

    Add modified plot3 files

--- plot3.R
+++ plot3.R
@@ -10,10 +10,12 @@ dat$Time <- strptime(dat$Time, "%d/%m/%Y %H:%M:%S")##convert to date/time POSIXc
 x <- dat$Sub_metering_1
 y <- dat$Sub_metering_2
 z <- dat$Sub_metering_3
+png(file = "plot3.png")
 plot(dat$Time, x, type = "n", xlab = "", ylab = "Energy Sub Metering")##prepare for plot
 points(dat$Time, x, type = "l", lty = 1, col = "black")
 points(dat$Time, y, type = "l", lty = 1, col = "red")
 points(dat$Time, z, type = "l", lty = 1, col = "blue")
 legend("topright",col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = 1)
-dev.copy(png, file = "plot3.png", width=480, height=480)## copy line graph to plot3.png
+##dev.copy(png, file = "plot3.png", width=480, height=480)## copy line graph to plot3.png
+
 dev.off()
